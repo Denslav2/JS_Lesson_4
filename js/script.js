34c55301bc8f790484b72ceaff8e6ae73450a1d5
Denslav2 <megajoomla@gmail.com>
2020-12-06 11:48:25 +0200

    Lesson_8

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+/*lesson_1.2.3.4.5.6
 let number = 5;
 const leftBorderWidth = 1;
 
@@ -25,4 +25,36 @@ const obj = {
 console.log(obj.name)
 
 let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
-console.log(arr[1]);
+console.log(arr[1]);*/
+
+
+
+/*Lesson_7
+alert('Hello');
+
+const result = confirm('Вы тут?')
+console.log(result);
+
+const answer = prompt("Вы уверенны?", "Es!");
+console.log(typeof(answer));
+
+//смена типа "Строка на число"//
+const answer = +prompt("Вы уверенны?", "");
+console.log(answer + 5);
+
+//Масивы//
+const answers = [];
+answers[0] = prompt("Как Вас зовут?", "");
+answers[1] = prompt("Как Ваша фемилия?", "");
+answers[2] = prompt("Сколько Вам лет?", "");
+
+document.write(answers);*/
+
+
+
+/*Lesson_8
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Ivan';
+alert(`Hello, ${user}`);/*
