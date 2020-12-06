909267811f8b150fdde9cf94c00ef587461dc49e
Denslav2 <megajoomla@gmail.com>
2020-12-06 10:42:36 +0200

    Lesson_6

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-console.log(10);
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(-4/0);
+console.log("string" * 9);
+
+const persone = "Alex";
+
+const bool = true;
+
+console.log(something);
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "Jone",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name)
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+console.log(arr[1]);
